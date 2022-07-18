025b760b8251ffe20b3a54c3f5f67b73b6df807f
Wladimir Silva <wladimir.sg@hotmail.com>
2022-07-18 18:46:10 -0400

    API Rest

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   apiUrl: 'https://62ce15a7066bd2b6992faf7d.mockapi.io/api/v1/'
 };
 
